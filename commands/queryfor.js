229c6c858b7b0b72ef8abb43d8a1339cfb472a2f
notmaiku <notmaiku@protonmail.com>
2019-06-07 21:34:22 -0400

    querying updated

--- commands/queryfor.js
+++ commands/queryfor.js
@@ -1,13 +1,15 @@
 exports.run = (client, message, args) => {
     const admin = require('firebase-admin');
     var db = admin.firestore();
-    var nameQuery = db.collection('servers').doc(`${message.guild.id}`).collection(`members`).where(`affection`, `>`, 1)
+    //You can use the firebase where clause but idk
+    var whereclause = db.collection('servers').doc(`${message.guild.id}`).collection(`members`).where(`${args}`, `>`, 0);
+    //This one doesn't use search query
+    var nameQuery = db.collection('servers').doc(`${message.guild.id}`).collection(`members`).orderBy('username');
     nameQuery.get().then(queryFor => {
-        console.log()
         message.channel.send(`There are ${queryFor.size} results`)
         if (queryFor.size > 0) {
             queryFor.forEach((doc) => {
-                if (doc.data().username) {
+                if (doc.data().affection && doc.data().username) {
                     message.channel.send(`${doc.data().username}: ${doc.data().affection}`);
                 }
             })
@@ -17,18 +19,43 @@ exports.run = (client, message, args) => {
     }).catch(err => {
         console.log('error')
     })
+
+    //Each member call the database
+    
+    // message.guild.members.forEach(member => {
+    //     var affectionQuery = db .collection('servers') .doc(`${message.guild.id}`) .collection(`members`).doc(`${member.user.id}`);
+    //     affectionQuery .get() .then(affectionProp => {
+    //             if (!member.user.bot) {
+    //                 if (affectionProp.data().affection > 1) {
+    //                     //ignoring the bots in the server.
+    //                     if (!member.user.bot) {
+    //                         message.channel.send(
+    //                             `${
+    //                                 affectionProp.data().username
+    //                             } affection is ${
+    //                                 affectionProp.data().affection
+    //                             }`
+    //                         );
+    //                     }
+    //                 }
+    //             }
+    //         })
+    //         .catch(err => {
+    //             console.log('Something went wrong');
+    //         });
+    // });
 };
 
 exports.conf = {
     enabled: true,
     guildOnly: false,
     aliases: [],
-    permLevel: "User"
+    permLevel: 'User'
 };
 
 exports.help = {
-    name: "Displays your filter",
-    category: "server",
-    description: "Will display all you filtered",
-    usage: "query"
+    name: 'Displays your filter',
+    category: 'server',
+    description: 'Will display all you filtered',
+    usage: 'query'
 };
--- commands/wipe.js
+++ commands/wipe.js
@@ -1,21 +1,21 @@
-exports.run = (client, message, args, member) => {
+exports.run = (message, guild) => {
     const admin = require('firebase-admin');
-    var FieldValue = require('firebase-admin').firestore.FieldValue;
-
 
     //references
     var db = admin.firestore();
-    var currentMembers = db.collection('servers').doc(`${message.guild.id}`).collection(`members`);
-    // Gets each member from guild it was called in 
-    var count = message.guild.members.forEach(member => {
-        // Actually deletes the field
-        var removeField = currentMembers.doc(`${member.user.id}`).update({
-            id: FieldValue.delete()
-        }).catch(err => {
-            console.log(`The ID doesn't have the field being deleted, so "Probably a bot`)
-        })
-    })
-}
+    //Members's affection field is instantiated 
+    var currentMembers = db.collection('servers').doc(`${guild.id}`).collection(`members`);
+    console.log(currentMembers)
+    currentMembers.forEach((member) => {
+        var currentMember = db.collection('servers').doc(`${guild.id}`).collection(`members`).doc(`${member.id}`);
+        currentMember.update({
+            gayness: FieldValue.delete()
+        });
+        message.channel.send('Wiped 😅')
+    }).catch(err => {
+        message.reply("Could not wipe server");
+    });
+};
 
 exports.conf = {
     enabled: true,
